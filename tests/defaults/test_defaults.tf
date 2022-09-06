46031605b2ef8fab9a7bf4f6075d1248b923ac41
Dustin Dortch <me@dustindoertch.com>
2022-09-06 18:06:39 -0400

    Added Terraform test

---
+++ tests/defaults/test_defaults.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.21.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_group_name  = "rg-test-virtual-network"
+  location             = "eastus"
+  virtual_network_name = "vnet-test-virtual-network"
+  address_space        = ["10.0.1.0/24"]
+}
+
+module "main" {
+  source = "../.."
+
+  name                = local.virtual_network_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = local.address_space
+
+  subnets = {
+    "default" = {
+      address_prefixes  = ["10.0.1.0/26"]
+      service_endpoints = ["Microsoft.Sql","Microsoft.Web"]
+
+      enforce_private_link_service_network_policies = true
+    }
+    "GatewaySubnet" = {
+      address_prefixes  = ["10.0.1.64/26"]
+    }
+    "AzureFirewallSubnet" = {
+      address_prefixes  = ["10.0.1.128/26"]
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+resource "test_assertions" "defaults" {
+  component = "defaults"
+
+  equal "name" {
+    description = "Virtual Network name is vnet-test-virtual-group"
+    got         = module.main.name
+    want        = local.virtual_network_name
+  }
+
+  equal "location" {
+    description = "Region is East US"
+    got         = module.main.location
+    want        = local.location
+  }
+
+  equal "address_space" {
+    description = "Virtual Network address space is [\"10.0.1.0/24\"]"
+    got         = module.main.address_space
+    want        = tolist(local.address_space)
+  }
+}
