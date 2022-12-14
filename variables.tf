9510aa182f480492393ed7d7e332937e8a2b4628
Dustin Dortch <25554532+dustindortch@users.noreply.github.com>
2022-12-14 01:06:05 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -45,5 +45,9 @@ variable "subnets" {
     enforce_private_link_endpoint_network_policies = optional(bool, false)
     enforce_private_link_service_network_policies  = optional(bool, false)
     service_endpoints                              = optional(list(string))
+    delegation                                     = optional(map(object({
+      service_name                                 = optional(string)
+      actions                                      = optional(list(string))
+    })))
   }))
 }
